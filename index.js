389a4f24394b79a4ba025d62240478acb20afbd1
Anitha Venkatesan <anithamca68@gmail.com>
2020-05-28 17:31:10 -0500

    added function for installation

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ let fs = require("fs");
 inquirer
   .prompt([
     {
-      message: "What is your GitHub Username?",
+      message: "Enter your GitHub Username?",
       name: "GitHubUsername"
     },
     {
-      message: "What is your GitHub Repository name?",
+      message: "Enter your GitHub Repository name?",
       name: "GitHubRepoName"
     },
     {
@@ -16,27 +16,23 @@ inquirer
       name: "title"
     },
     {
-        message: "What is Description about the project?",
+        message: "Write Description about the project?",
         name: "description"
     },
     {
-        message: "What is Table of Contents?",
-        name: "tableOfContent"
+        message: "How many steps in Installation?",
+        name: "installation",
     },
     {
-        message: "Installation?",
-        name: "installation"
-    },
-    {
-        message: "Usage?",
+        message: "How to execute the application?",
         name: "usage"
     },
     {
-        message: "License?",
+        message: "What is the License name?",
         name: "license"
     },
     {
-        message: "Contributing?",
+        message: "How many contributors in this project?",
         name: "contibuting"
     },
     {
@@ -53,28 +49,56 @@ inquirer
     }
   ]).then(function(data) {
       const readMeDetails = `
-      ${data.GitHubUsername}
-      ${data.GitHubRepoName}
-      ${data.title}
-      ${data.description}
-      ${data.tableOfContent}
-      ${data.installation}
-      ${data.usage}
-      ${data.license}
-      ${data.contibuting}
-      ${data.tests}
-      ${data.profile}
-      ${data.email}
-      `;
+## ${data.GitHubUsername}
+## ${data.GitHubRepoName}
+## ${data.title}
+## Description
+* ${data.description}
+## Table of Contents 
+    [Installation](Installation)
+    [Usage](Usage)
+    [License](License)
+    [Contributor](Contributor)
+   [Tests](Tests)
+    [Questions](Questions)   
+### Installation
+* ${installation()}
+### Usage
+* ${data.usage}
+### License
+* ${data.license}
+### Contributor
+* ${data.contibuting}
+### Tests
+* ${data.tests}
+### Questions
+* ${data.profile}
+* ${data.email}
+            `;
 
       
-     fs.writeFile("utils/generateMarkdown.js", readMeDetails, function(err) {
+     fs.writeFile("utils/README.md", readMeDetails, function(err) {
   
         if (err) {
             return console.log(err);
           }
           else console.log("Succesfully Writing into the html file");
-    });
+    }); 
     
+    async function installation()
+    {
+      let stepLimit = data.installation
+    if(!Number(stepLimit))
+      {
+        return;
+      }
+    else {
+        for(let i=1;i<= stepLimit;i++)
+        {
+          console.log("Enter the installation steps one by one?")
+        }
+       }
+    } 
   }); 
-    
+
+  
