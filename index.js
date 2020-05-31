6549c32e5f62635a173c313c7d20578665892129
Anitha Venkatesan <anithamca68@gmail.com>
2020-05-30 19:24:17 -0500

    called functions from seperate folder

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 let inquirer = require("inquirer");
-let fs = require("fs");
 let lodash = require('lodash');
 const axios = require("axios");
+const markdown =require('./utils/generateMarkdown');
 const apis = require('./utils/axiosGitHubApi');
 
+//inquirer to get command line input
 inquirer
   .prompt([
     {
@@ -47,8 +48,7 @@ inquirer
         name: "description",
         default :"Project description"
         
-    },
-    
+    },   
     {
         message: "What command should be used to install npm packages ?",
         name: "installation",
@@ -70,68 +70,19 @@ inquirer
       message: "What is the license used in this project?",
       name: "license",
       choices: ['MIT', 'BSL','GPL'],
-    }         
+    } 
+           
   ]).then(async function(data) {
+    try{
+      const profile = await apis.getGitHubProfile(data.gitHubUsername);
+      const repo = await apis.getGitHubRepo(data.gitHubUsername,data.gitHubRepoName);
+      markdown.generateMarkdown(data, profile, repo); 
+      
+    }
+    catch(err) {
+      console.log(err);
+    }
     
-    let licenseContent = `[${data.license}]`;
-    if (licenseContent === 'MIT') {
-      licenseContent += '(https://choosealicense.com/licenses/mit/)';
-    } else if (licenseContent === 'BSL'){
-      licenseContent += '(https://choosealicense.com/licenses/bsl-1.0/)';
-    } else if(licenseContent === 'GPL') {
-      licenseContent += '(https://choosealicense.com/licenses/gpl-3.0/)';
-    }else {
-      licenseContent += '(https://choosealicense.com/licenses/mit/)';
-    }
-
-    const profile = await apis.getGitHubProfile(data.gitHubUsername);
-    const repo = await apis.getGitHubRepo(data.gitHubUsername,data.gitHubRepoName)
-  
-
-    const content = [ 
-      '### License',
-      '* This program is licensed under the ' +licenseContent +' license.' 
-    ];
-
-  const readMeDetails = `
-# ${data.title}
-![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
-[![npm version](https://badge.fury.io/js/cli.svg)](https://badge.fury.io/js/cli)
-[![npm version](https://badge.fury.io/js/lodash.svg)](https://badge.fury.io/js/lodash)
-## Description
-* ${data.description}
-## Table of Contents 
-    \n 1. [Installation](#installation)
-    \n 2. [Usage](#usage)
-    \n 3. [Contributing](#contributing)
-    \n 4. [License](#license)
-    \n 5. [Tests](#tests)
-    \n 6. [Questions](#questions)
-### Installation
-* \`${data.installation}\` \n
-### Usage
-* ${data.usage} \n
-### Contributing \n
-* Fork the repository,
-* Clone the repository using git clone, 
-* Implement your code,
-* Create a Pull Request, 
-* Once approved, it will merge to master.
-### Tests 
-* \`${data.tests}\` \n
-${content.join('\n')}
-### Questions
-* If you have any questions, Please feel free to contact me [${profile.name}](${profile.html_url}) on my email ${data.GitHubEmail}\n
-* You can also create [issues](${repo.html_url}/issues) on my repo.
-\n ![My profile image](${profile.avatar_url}&s=200)
-`;
-
-
-  fs.writeFileSync("utils/README.md", readMeDetails, function(err) {
-    if (err) {
-      return console.log(err);
-    }
-  });
- 
+   
 });
   
